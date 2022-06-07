1192908496e21d5f90d4cd066d3202e1c3fdb3d9
Mychael Dourado <mychaeldourado23@gmail.com>
2022-06-07 19:06:28 -0300

    Adding the hide menu with links and scrolling header interaction.

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,22 @@ navClose.addEventListener('click', () => {
   navMenu.classList.remove('show-menu');
 
 })
+
+const navLink = document.querySelectorAll('.nav__link');
+
+const hideMenu = () => {
+
+  navMenu.classList.remove('show-menu');
+
+}
+
+navLink.forEach(link => link.addEventListener('click', hideMenu))
+
+const scrollHeader = () => {
+
+  const header = document.getElementById('header')
+
+  if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+
+}
+window.addEventListener('scroll', scrollHeader);
