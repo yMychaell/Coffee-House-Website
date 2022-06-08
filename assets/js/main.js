06dd7575ae7d553defadc669b6a620c1ffea892e
Mychael Dourado <mychaeldourado23@gmail.com>
2022-06-08 16:19:42 -0300

    Adding the sections interaction with menu and adding the products filter with mixItUp.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close');
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 navToggle.addEventListener('click', () => {
 
@@ -28,7 +28,53 @@ const scrollHeader = () => {
 
   const header = document.getElementById('header')
 
-  if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+  if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 
 }
 window.addEventListener('scroll', scrollHeader);
+
+let mixerProduct = mixitup('.products__content', {
+  selectors: {
+    target: '.products__card'
+  },
+  animation: {
+    duration: 300
+  }
+});
+mixerProduct.filter('.delicacies')
+
+const categoryProduct = document.querySelectorAll('.products__item');
+
+function activeProduct() {
+
+  categoryProduct.forEach(category => category.classList.remove('active-product'))
+  this.classList.add('active-product');
+
+}
+categoryProduct.forEach(category => category.addEventListener('click', activeProduct))
+
+const section = document.querySelectorAll('section[id]');
+
+function activeSectionMenu() {
+
+  const scrollY = window.pageYOffset
+
+  section.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else {
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+
+    console.log(section)
+
+  })
+}
+window.addEventListener('scroll', activeSectionMenu)
+
+
+
